fcd8deab1a87ff4f46e3b34312d1b258f35397c0
tailtarget <tailtarget@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-30 20:17:51 +0000

    Update interface plugin
    git-svn-id: https://plugins.svn.wordpress.org/tailtarget/trunk@1079206 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tailtarget.php
+++ tailtarget.php
@@ -1,17 +1,19 @@
 <?php
 /*
- * Plugin Name: Tail Target for Wordpress 
+ * Plugin Name: TailTarget DMP 
  * Plugin URI: http://www.tailtarget.com/dev/wordpress
- * Description: Utilizando o plugin da Tail Target, você terá um entendimento qualitativo dos usuários que navegam pelo seu site, para utilizar você precisa ter uma conta na <a href="http://tailtarget.com/" target="_blank">Tail Target</a>. <strong>Cadastre-se Gratuitamente <a href="http://www.tailtarget.com" target="_blank">Aqui</a></strong>
- * Version: 0.0.1
+ * Description: Utilizando o plugin da Tail Target, você terá um entendimento qualitativo dos usuários que navegam pelo seu site, para utilizar você precisa ter uma conta na <a href="http://www.tailtarget.com/" target="_blank">Tail Target</a>. <strong>Cadastre-se Gratuitamente <a href="http://www.tailtarget.com" target="_blank">Aqui</a></strong>
+ * Version: 1.0
  * Author: Tail Target
- * Author URI: http://tailtarget.com/
+ * Author URI: http://www.tailtarget.com/
  * License: GPLv2
  * Text Domain: tailtarget
  */
 
-include('includes/Tracking.php');
 
-if ( class_exists('trackingTailTarget') ) {
-	$ttTracking = new trackingTailTarget();
-}
+include('includes/Settings.php');
+include('includes/Translate.php');
+include('includes/Scripts.php');
+include('includes/Layout.php');
+include('includes/Connect.php');
+include('includes/Option.php');
